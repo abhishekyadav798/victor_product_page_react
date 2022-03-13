337006c36243c86d2ccba24a37a4b2dcd7abdbd0
abhishekyadav798 <99576491+abhishekyadav798@users.noreply.github.com>
2022-03-13 16:57:26 +0530

    updated App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import {Row,Col} from 'reactstrap'
 import './App.css';
 import Bottom_bar from './Bottom_bar';
-
+import Carousel_top from './Carousel_top';
 import Column_fixed_and_scroll from './Column_fixed_and_scroll';
-
+import Expand_onclick_div from './Expand_onclick_div';
+import Fixed_2 from './Fixed_2';
+import Fixed_3 from './Fixed_3';
+import Fixed_component from './Fixed_component';
 import Footer from './Footer';
+import Footer_2nd_top from './Footer_2nd_top';
+import Footer_lowest from './Footer_lowest';
 import Navbar_top from './Navbar_top';
+import Review_and_rating from './Review_and_rating';
 // import Navbar1 from './Navbar1';
-
+import Scrollspy_component from './Scrollspy_component';
+import Scrollspy_html from './Scrollspy_html';
+import Side_component_fixed from './Side_component_fixed';
+import Single_component_hide_and_scroll from './Single_component_hide_and_scroll';
 
 function App() {
   return (
@@ -16,7 +25,7 @@ function App() {
       
       <Row>
         <Col>
-        <Column_fixed_and_scroll/>
+        <Single_component_hide_and_scroll/>
         </Col>
       </Row>
       <Row>
@@ -24,6 +33,12 @@ function App() {
       </Row>
       <Row>
         <Bottom_bar/>
+      </Row>
+      <Footer_2nd_top/>
+      <Footer_lowest/>
+      
+      <Row>
+        
       </Row>
  
     
